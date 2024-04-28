01bf64bfc8acfa8f631505a3146552a4a4836207
ShaMan123 <shacharnen@gmail.com>
2024-04-28 08:38:47 +0300

    improve

--- codegen.ts
+++ codegen.ts
@@ -1,15 +1,16 @@
 import {
   ElementHandle,
-  JSHandle,
   Page,
   PageScreenshotOptions,
   TestInfo,
   expect,
 } from "@playwright/test";
 import { EventEmitter } from "events";
+import { readFileSync, writeFileSync } from "fs";
 import { ensureFileSync } from "fs-extra";
+import capitalize from "lodash.capitalize";
+import debounce from "lodash.debounce";
 import { execSync } from "node:child_process";
-import { appendFileSync } from "node:fs";
 import * as path from "node:path";
 
 const EVENT_MODIFIER_KEYS = [
@@ -20,18 +21,44 @@ const EVENT_MODIFIER_KEYS = [
 ] as const;
 type EventModifiers = Record<(typeof EVENT_MODIFIER_KEYS)[number], boolean>;
 
-const EVENT_KEYS = ["type", "x", "y", "key"] as const;
-
-const EVENTS = ["mousedown", "mousemove", "mouseup", "dblclick"] as const;
-type EventType = (typeof EVENTS)[number];
-
-type MouseEventData = {
-  type: EventType;
+const MOUSE_EVENTS = ["mousedown", "mousemove", "mouseup", "dblclick"] as const;
+type MouseEventType = (typeof MOUSE_EVENTS)[number];
+type BasicMouseEventData = {
   x: number;
   y: number;
   handle: string;
 } & Partial<EventModifiers>;
-// type KeyboardEventData = { key: string } & Partial<EventModifiers>;
+type MouseEventData = {
+  type: MouseEventType;
+} & BasicMouseEventData;
+type MouseMoveEventData = BasicMouseEventData & {
+  type: "mousemove";
+  steps?: number;
+};
+type ConsumedMouseEventData = (
+  | {
+      type: "mousedown" | "mouseup" | "dblclick";
+    }
+  | {
+      type: "mousemove";
+      steps?: number;
+    }
+  | { type: "click" }
+) &
+  BasicMouseEventData;
+
+const KEYBOARD_EVENTS = ["keydown", "keyup"] as const;
+type KeyboardEventType = (typeof KEYBOARD_EVENTS)[number];
+type BasicKeyboardEventData = {
+  key: string;
+} & Partial<EventModifiers>;
+type KeyboardEventData = BasicKeyboardEventData & {
+  type: KeyboardEventType;
+};
+type ConsumedKeyboardEventData = BasicKeyboardEventData & {
+  type: KeyboardEventType | "keypress";
+};
+
 type ScreenshotEventData = {
   type: "screenshot";
   handle: string;
@@ -39,16 +66,27 @@ type ScreenshotEventData = {
   name?: string;
   options?: PageScreenshotOptions;
 };
+
 type StepEventData = { type: "step"; which: "start" | "end"; name?: string };
-type EventData =
-  | MouseEventData
-  // | KeyboardEventData
+
+type EventMap = {
+  [K in MouseEventType]: MouseEventData;
+} & { [K in KeyboardEventType]: KeyboardEventData } & {
+  screenshot: ScreenshotEventData;
+  step: StepEventData;
+};
+type ConsumedEventData =
+  | ConsumedMouseEventData
+  | ConsumedKeyboardEventData
   | ScreenshotEventData
   | StepEventData;
 
-type R = {
-  [K in EventType]: MouseEventData;
-} & { screenshot: ScreenshotEventData; step: StepEventData };
+const EVENTS_TO_CONSUME = [
+  ...MOUSE_EVENTS,
+  ...KEYBOARD_EVENTS,
+  "step",
+  "screenshot",
+] as const;
 
 const getCodegenKey = ({
   key,
@@ -56,28 +94,27 @@ const getCodegenKey = ({
   ctrlKey,
   metaKey,
   shiftKey,
-}: Partial<EventModifiers & { key: string }>) => {
-  return Object.entries({ altKey, ctrlKey, metaKey, shiftKey, key })
-    .map(([key, value]) => {
-      if (!value) {
-        return "";
-      }
-      const [upper, ...rest] = key.replace("Key", "");
-      return `${upper.toUpperCase()}${rest}`;
-    })
+}: Partial<EventModifiers> & { key: string }) => {
+  return Object.entries({ altKey, ctrlKey, metaKey, shiftKey })
+    .map(([key, value]) => (value ? capitalize(key.replace("Key", "")) : ""))
     .filter((val) => !!val && val !== key)
+    .concat(key)
     .join("+");
 };
 
 export class Codegen extends EventEmitter {
   readonly testInfo: TestInfo;
-  protected events: EventData[] = [];
-  private recording = false;
-  private counter = 0;
   private readonly file: string;
+  protected events: ConsumedEventData[] = [];
+  private recording = false;
+  private down = false;
+  private counter = 0;
 
   static async start(page: Page, testInfo: TestInfo) {
     const codegen = new Codegen(testInfo);
+    try {
+      execSync(`code ${codegen.file}`);
+    } catch (error) {}
     await codegen.install(page);
     await page.evaluate(() => startRecording());
     await page.pause();
@@ -87,82 +124,86 @@ export class Codegen extends EventEmitter {
   constructor(testInfo: TestInfo) {
     super();
     this.testInfo = testInfo;
-
-    this.file = path.resolve(testInfo.outputDir, "codegen.ts");
-    ensureFileSync(this.file);
-    appendFileSync(
-      this.file,
-      [
-        `import { test, expect, Page } from "@playwright/test";`,
-        "",
-        "",
-        "let page: Page; // here just for TS",
-        "",
-        "// you may need to replace the `selector` value",
-        `const selector = 'canvas_top=#canvas';`,
-        "",
-        "",
-      ].join("\n")
-    );
-    testInfo.attach("codegen.ts", {
-      path: this.file,
-    });
-
-    ([...EVENTS, "step", "screenshot"] as const).forEach((eventType) =>
-      this.on(eventType, (ev) => {
-        this.events.push(ev);
-        appendFileSync(this.file, this.parse(ev).concat("").join("\n"));
-      })
-    );
-    this.on("screenshot", (ev) => this.attachScreenshot(ev));
+    this.file = this.prepareFile(testInfo);
+    this.attach();
   }
 
-  on<K extends keyof R>(eventType: K, listener: (ev: R[K]) => any) {
-    return super.on(eventType, listener);
+  protected prepareFile(testInfo: TestInfo) {
+    const file = path.resolve(testInfo.outputDir, "codegen.ts");
+    ensureFileSync(file);
+    testInfo.attach("codegen.ts", {
+      path: file,
+    });
+
+    return file;
+  }
+
+  toSelector(handle: ElementHandle | null) {
+    return "selector";
   }
 
   async install(page: Page) {
-    try {
-      execSync(`code ${this.file}`);
-    } catch (error) {}
-
     await page.exposeFunction(
       "resolveSelector",
       (handle: ElementHandle<Node>) => this.toSelector(handle)
     );
 
-    await page.exposeFunction("consumeEvent", async (e: MouseEventData) => {
-      this.emit(e.type, {
-        ...e,
-      } as MouseEventData);
-    });
+    await page.exposeFunction(
+      "emitCodegenEvent",
+      async (e: MouseEventData | KeyboardEventData) => {
+        this.recording && this.emit(e.type, e);
+      }
+    );
 
-    await page.exposeBinding("startRecording", async ({ page }) => {
+    const disposer = await page.evaluateHandle(
+      ([mouseEvents, keyboardEvents, modifiers]) => {
+        const consumeMouseEvent = async (e: MouseEvent) =>
+          emitCodegenEvent({
+            type: e.type,
+            x: e.x,
+            y: e.y,
+            ...Object.fromEntries(
+              modifiers.map((k) => [k, e[k]]).filter(([k, v]) => !!v)
+            ),
+            handle: await resolveSelector(e.target),
+          });
+        const consumeKeyboardEvent = async (e: KeyboardEvent) =>
+          emitCodegenEvent({
+            type: e.type,
+            key: e.key,
+            ...Object.fromEntries(
+              modifiers.map((k) => [k, e[k]]).filter(([k, v]) => !!v)
+            ),
+            handle: await resolveSelector(e.target),
+          });
+
+        mouseEvents.forEach((ev) =>
+          window.addEventListener(ev, consumeMouseEvent)
+        );
+        keyboardEvents.forEach((ev) =>
+          window.addEventListener(ev, consumeKeyboardEvent)
+        );
+
+        return () => {
+          mouseEvents.forEach((ev) =>
+            window.removeEventListener(ev, consumeMouseEvent)
+          );
+          keyboardEvents.forEach((ev) =>
+            window.removeEventListener(ev, consumeKeyboardEvent)
+          );
+        };
+      },
+      [MOUSE_EVENTS, KEYBOARD_EVENTS, EVENT_MODIFIER_KEYS] as const
+    );
+    // await disposer.evaluate((d) => d());
+    // await disposer.dispose();
+
+    await page.exposeFunction("startRecording", () => {
       this.recording = true;
-      const disposer = await page.evaluateHandle(
-        ([events, modifiers]) => {
-          const consume = async (e: MouseEvent) =>
-            consumeEvent({
-              type: e.type,
-              x: e.x,
-              y: e.y,
-              ...Object.fromEntries(
-                modifiers.map((k) => [k, e[k]]).filter(([k, v]) => !!v)
-              ),
-              handle: await resolveSelector(e.target),
-            });
-          events.forEach((ev) => window.addEventListener(ev, consume));
-          return () =>
-            events.forEach((ev) => window.removeEventListener(ev, consume));
-        },
-        [EVENTS, EVENT_MODIFIER_KEYS] as const
-      );
     });
 
-    await page.exposeFunction("stopRecording", async () => {
+    await page.exposeFunction("stopRecording", () => {
       this.recording = false;
-      // await disposer.evaluate((d) => d());
-      // await disposer.dispose();
     });
 
     const assertRecording = () => {
@@ -206,6 +247,13 @@ export class Codegen extends EventEmitter {
     );
   }
 
+  protected attach() {
+    const write = debounce(() => this.write(), 250);
+    const exec = (ev: EventMap[keyof EventMap]) => this.consume(ev) && write();
+    EVENTS_TO_CONSUME.forEach((eventType) => this.on(eventType, exec));
+    this.on("screenshot", (ev) => this.attachScreenshot(ev));
+  }
+
   protected attachScreenshot({ name, screenshot }: ScreenshotEventData) {
     this.testInfo.attach(name || `codegen${this.counter++}.png`, {
       body: screenshot,
@@ -218,122 +266,153 @@ export class Codegen extends EventEmitter {
     this.testInfo.config.updateSnapshots = updateSnapshots;
   }
 
-  toSelector(handle: ElementHandle | null) {
-    return "selector";
+  on<K extends keyof EventMap>(
+    eventType: K,
+    listener: (ev: EventMap[K]) => any
+  ) {
+    return super.on(eventType, listener);
   }
 
-  parse(ev: EventData) {
-    const data = this.events.slice();
-    const index = data.length - 1;
-    switch (ev.type) {
-      // case "keydown": {
-      //   {
-      //     if (array[index + 1]?.[0] !== "keyup") {
-      //       const key = getCodegenKey(ev as KeyboardEventData);
-      //       return [`await page.keyboard.down('${key}')`];
-      //     }
-      //     return [];
-      //   }
-      // }
-      // case "keyup": {
-      //   {
-      //     const key = getCodegenKey(ev as KeyboardEventData);
-      //     if (array[index - 1]?.[0] === "keydown") {
-      //       return [`await page.keyboard.press('${key}')`];
-      //     }
-      //     return [`await page.keyboard.up('${key}')`];
-      //   }
-      // }
+  consume(ev: EventMap[keyof EventMap]) {
+    const last = this.events[this.events.length - 1]?.type;
 
-      case "mouseup": {
-        const { x, y, handle } = ev;
-        if (data[index - 1]?.[0] === "mousedown") {
+    if (ev.type === "mousedown") {
+      this.down = true;
+    } else if (ev.type === "mouseup") {
+      this.down = false;
+    } else if (ev.type === "mousemove" && !this.down) {
+      return false;
+    }
+
+    if (ev.type === "mousemove" && last === "mousemove") {
+      const { steps = 0 } = this.events.pop() as MouseMoveEventData;
+      this.events.push({ ...ev, type: "mousemove", steps: steps + 1 });
+    } else if (ev.type === "mouseup" && last === "mousedown") {
+      this.events.pop();
+      this.events.push({ ...ev, type: "click" });
+    } else if (ev.type === "keyup" && last === "keydown") {
+      this.events.pop();
+      this.events.push({ ...ev, type: "keypress" });
+    } else {
+      this.events.push(ev);
+    }
+
+    return true;
+  }
+
+  parse() {
+    const data = this.events.slice();
+
+    // close last step
+    const lastStep = data.findLast((ev) => ev.type === "step") as
+      | StepEventData
+      | undefined;
+    lastStep?.which === "start" && data.push({ type: "step", which: "end" });
+
+    return data.flatMap((ev) => {
+      switch (ev.type) {
+        case "keydown": {
+          return [`await page.keyboard.down('${getCodegenKey(ev)}');`];
+        }
+
+        case "keyup": {
+          return [`await page.keyboard.up('${getCodegenKey(ev)}');`];
+        }
+
+        case "keypress": {
+          return [`await page.keyboard.press('${getCodegenKey(ev)}');`];
+        }
+
+        case "mousedown": {
+          const { x, y, handle } = ev as MouseEventData;
+          return [
+            `await page.hover(${handle}, ${JSON.stringify({
+              position: { x, y },
+            })});`,
+            `await page.mouse.down();`,
+          ];
+        }
+
+        case "mousemove": {
+          const { x, y, steps = 0 } = ev;
+          return [`await page.mouse.move(${x}, ${y}, { steps: ${steps} });`];
+        }
+
+        case "mouseup": {
+          return [`await page.mouse.up();`];
+        }
+
+        case "click": {
+          const { x, y, handle } = ev;
           return [
             `await page.click(${handle}, ${JSON.stringify({
               position: { x, y },
             })});`,
           ];
         }
-        const downIndex = data.findLastIndex((ev) => ev.type === "mousedown");
-        const down = data[index] as MouseEventData;
-        const move = data
-          .slice(downIndex + 1, index)
-          .filter((ev) => ev.type === "mousemove") as MouseEventData[];
-        return [
-          `await page.hover(${down.handle}, ${JSON.stringify({
-            position: { x: down.x, y: down.y },
-          })});`,
-          `await page.mouse.down();`,
-          ...move
-            .reduce(
-              (ev, curr) => {
-                const last = ev[ev.length - 1];
-                return EVENT_MODIFIER_KEYS.every((k) => curr[k] === last[k])
-                  ? ev.slice(0, -1).concat({ ...curr, steps: last.steps + 1 })
-                  : ev.concat({ ...curr, steps: 0 });
-              },
-              [{ ...move[0], steps: 0 }]
-            )
-            .map(
-              ({ x, y, steps }) =>
-                `await page.mouse.move(${x}, ${y}, { steps: ${steps} });`
-            ),
-          `await page.mouse.up();`,
-        ];
-      }
 
-      case "dblclick": {
-        const { x, y } = ev;
-        return [`await page.mouse.dblclick(${x}, ${y})`];
-      }
-
-      case "screenshot": {
-        const { name, options, handle } = ev;
-        return [
-          `expect(await page.locator(${handle}).screenshot(${
-            options ? JSON.stringify(options) : ""
-          })).toMatchSnapshot(${name ? JSON.stringify({ name }) : ""});`,
-        ];
-      }
-
-      case "step": {
-        const { name, which } = ev;
-        if (which === "start") {
-          return [`await test.step('${name || "step"}', async () => {`];
-        } else {
-          return [`});`];
+        case "dblclick": {
+          const { x, y } = ev;
+          return [`await page.mouse.dblclick(${x}, ${y});`];
         }
-      }
 
-      default:
-        return [];
-    }
+        case "screenshot": {
+          const { name, options, handle } = ev;
+          return [
+            `expect(await page.locator(${handle}).screenshot(${
+              options ? JSON.stringify(options) : ""
+            })).toMatchSnapshot(${name ? JSON.stringify({ name }) : ""});`,
+          ];
+        }
+
+        case "step": {
+          const { name, which } = ev;
+          if (which === "start") {
+            return [`await test.step('${name || "step"}', async () => {`];
+          } else {
+            return [`});`];
+          }
+        }
+
+        default:
+          return [];
+      }
+    });
   }
 
-  // attachCode() {
+  write() {
+    writeFileSync(
+      this.file,
+      [
+        `import { test, expect } from "@playwright/test";`,
+        "",
+        "",
+        "test('codegen output', async ({ page }, testInfo) => {",
+        "",
+        "// you may need to replace the `selector` value",
+        `const selector = '#canvas';`,
+        "",
+        "",
+        ...this.parse(),
+        "});",
+      ].join("\n")
+    );
 
-  //   // close last step
-  //   const lastStep = data.findLast((ev) => ev.type === "step") as
-  //   | StepEventData
-  //   | undefined;
-  // lastStep.which === "start" && data.push({ type: "step", which: "end" });
-  //   this.testInfo.attach("recorded events", {
-  //     body: JSON.stringify(data, null, 2),
-  //   });
-  //   const pathToFile = path.resolve(testInfo.outputDir, "codegen.ts");
-  //   writeFileSync(pathToFile, codegen);
-  //   testInfo.attach("codegen.ts", {
-  //     path: pathToFile,
-  //   });
-  //   execSync(`prettier --write --ignore-path '' ${pathToFile}`);
-  //   const body = readFileSync(pathToFile).toString();
-  //   testInfo.attach("codegen", {
-  //     body,
-  //   });
-  //   console.log(
-  //     `\n\nCodegen of the test "${testInfo.title}" has completed successfully`,
-  //     "Generated output is available in the test attachments\n\n",
-  //     body
-  //   );
-  // }
+    try {
+      execSync(`prettier --write --ignore-path '' ${this.file}`);
+    } catch (error) {}
+  }
+
+  attachOutput() {
+    this.testInfo.attach("recorded events", {
+      body: JSON.stringify(this.events, null, 2),
+    });
+    this.testInfo.attach("codegen", {
+      body: readFileSync(this.file).toString(),
+    });
+    console.log(
+      `\n\nCodegen of the test "${this.testInfo.title}" has completed successfully`,
+      "Generated output is available in the test attachments\n\n"
+    );
+  }
 }
